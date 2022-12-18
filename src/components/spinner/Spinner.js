4f58c3c496aff88b2456ef41f0942264f4a09bf0
JanBadon <jan.badon.makaron@gmail.com>
2022-12-18 22:53:15 +0300

    ...

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,7 +1,8 @@
+import './spinner.scss';
 
 const Spinner = () => {
     return (
-        <div className="spinner-border mx-auto mt-5" role="status">
+        <div className="spinner" role="status">
             <span className="visually-hidden">Loading...</span>
         </div>
     )
